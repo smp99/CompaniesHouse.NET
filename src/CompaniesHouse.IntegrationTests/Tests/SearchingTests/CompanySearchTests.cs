14c0f12af58cf732b7c90d6146dd18d3951dc7c2
kevbite <kev_bite@msn.com>
2018-10-11 15:15:30 +0100

    Adding _Learning SPACE Belfast Ltd_ as a search test.

--- src/CompaniesHouse.IntegrationTests/Tests/SearchingTests/CompanySearchTests.cs
+++ src/CompaniesHouse.IntegrationTests/Tests/SearchingTests/CompanySearchTests.cs
@@ -7,6 +7,7 @@ namespace CompaniesHouse.IntegrationTests.Tests.SearchingTests
 {
     [TestFixture("brighouse computers")]
     [TestFixture("British Gas")]
+    [TestFixture("Learning SPACE Belfast Ltd")]
 	public class CompanySearchTests
     {
         private readonly string _query;
